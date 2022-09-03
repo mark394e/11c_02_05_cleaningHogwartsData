ba7f06066bf4e62fb54622740b84236eb7c73227
Markus <mark394e@stud.kea.dk>
2022-09-03 19:33:35 +0200

    comments

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 document.addEventListener("DOMContentLoaded", init);
 
+// creating empty array for cleaned up data
 const studentArray = [];
 
 const JSONArray = "https://petlatkea.dk/2021/hogwarts/students.json";
 
+// creating object-protorype for data
 const Student = {
   firstname: "",
   nickname: "",
@@ -22,6 +24,7 @@ async function init() {
   loadStudents();
 }
 
+// fetching data from JSON and logging array in a table
 async function loadStudents() {
   const response = await fetch(JSONArray);
   const studentList = await response.json();
@@ -30,31 +33,44 @@ async function loadStudents() {
 }
 
 function prepareStudents(studentList) {
+  // for each elm/student in the array, create a new object with the Student-object-prototype
   studentList.forEach((elm) => {
     const student = Object.create(Student);
 
+    // trim every name, house and gender - making sure there's no space in front or behind
     let fullName = elm.fullname.trim();
     let house = elm.house.trim();
     let gender = elm.gender.trim();
 
+    // defining the firstname property. capitalises the first letter in the first name and lowering the rest
     student.firstname = fullName.substring(0, 1).toUpperCase() + fullName.substring(1, fullName.indexOf(" ")).toLowerCase();
 
+    // if the fullName includes " then define the word betwen the two " as a nickname as well as capitalising it
+    // as well as defining the nickname property
     if (fullName.includes(`"`)) {
       student.nickname = fullName.substring(fullName.indexOf(`"`) + 1, fullName.indexOf(`"`) + 2).toUpperCase() + fullName.substring(fullName.indexOf(`"`) + 2, fullName.lastIndexOf(`"`)).toLowerCase();
+      // removing the name from the middelname because its a nickname
       student.middelname = "";
-      console.log(student.middelname);
     }
 
+    // if the fullName does not include a space then capitalise the first letter in the name and lower the rest
     if (fullName.includes(" ") === false) {
       student.firstname = fullName.substring(0, 1).toUpperCase() + fullName.substring(1).toLowerCase();
     }
 
+    // defining the middelname property. trimming the middelname and capitalising the first letter and lowering the rest
     student.middelname = fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" ")).trim().substring(0, 1).toUpperCase() + fullName.substring(fullName.indexOf(" "), fullName.lastIndexOf(" ")).trim().substring(1).toLowerCase();
+
+    // defining the lastname property. capitalising the first letter in lastname and lowering the rest
     student.lastname = fullName.substring(fullName.lastIndexOf(" ") + 1, fullName.lastIndexOf(" ") + 2).toUpperCase() + fullName.substring(fullName.lastIndexOf(" ") + 2).toLowerCase();
 
+    // defining the gender property. capitalising the first letter in gender and lowering the rest
     student.gender = gender.substring(0, 1).toUpperCase() + gender.substring(1).toLowerCase();
+
+    // defining the house property. capitalising the first letter in gender and lowering the rest
     student.house = house.substring(0, 1).toUpperCase() + house.substring(1).toLowerCase();
 
+    // adding all the student-objects to the student array
     studentArray.push(student);
   });
 
@@ -62,5 +78,6 @@ function prepareStudents(studentList) {
 }
 
 function showAllStudents() {
+  // displaying the student array as a table in the console
   console.table(studentArray);
 }
